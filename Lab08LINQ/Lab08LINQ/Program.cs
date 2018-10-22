1384f7a0331c3012948f1ce722358743b94b6e16
thejamilah <jamilahmcw@gmail.com>
2018-10-22 09:02:59 -0700

    init vs file

---
+++ Lab08LINQ/Lab08LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab08LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
