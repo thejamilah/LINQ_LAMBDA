42dba4d104c4a8194bb79f30c2b4f99abfc0d3a2
thejamilah <jamilahmcw@gmail.com>
2018-10-30 16:07:20 -0700

    Commit before working on standard LINQuery

--- Lab08LINQ/Lab08LINQ/Program.cs
+++ Lab08LINQ/Lab08LINQ/Program.cs
@@ -1,15 +1,14 @@
 ﻿using Lab08LINQ.Classes;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
 
 namespace Lab08LINQ
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("LINQ in Manhatten");
             Console.WriteLine("==========");
@@ -36,9 +35,67 @@ namespace Lab08LINQ
             {
                 Console.WriteLine(item);
             }
+
             //Separation between next query
+            Console.WriteLine(" ");
             Console.WriteLine("====================");
+            Console.WriteLine(" ");
+
+            //All neighborhoods; do not print empty lines
+            //Of the Lab assignment this satifies Query 2. Filter out all the neighborhoods that do not have any names
+            var removeEmptyNeighborhood = allNeighborhood.Where(x => x != "");
+
+            foreach (var item in removeEmptyNeighborhood)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Separation between next query
+            Console.WriteLine(" ");
+            Console.WriteLine("===================");
+            Console.WriteLine(" ");
+
+            //Remove neighborhood using .Distint() reference https://www.dotnetperls.com/distinct for future reference
+            //Of the Lab assignment this satifies Query 3. Remove the Duplicates
+            var distinctNeighborhood = removeEmptyNeighborhood.Distinct();
+
+            foreach (var item in distinctNeighborhood)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Separation between next query
+            Console.WriteLine(" ");
+            Console.WriteLine("===================");
+            Console.WriteLine(" ");
+
+            //Consolidate 
+            //Of the Lab assignment this satifies Query 4. Rewrite the queries from above, and consolidate all into one single query.
+            var consolidateData = Data.Features.Select(x => x)
+                                               .Select(x => x.Properties.neighborhood)
+                                               .Where(x => x != "")
+                                               .Distinct();
+
+            foreach (var item in consolidateData)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Separation between next query
+            Console.WriteLine(" ");
+            Console.WriteLine("====================");
+            Console.WriteLine(" ");
+
+            //Use LINQ instead of Lamda => expressions
+            //Of the Lab assignment this satifies 5. Rewrite at least one of these questions only using the opposing method (example: Use LINQ instead of a Lambda and vice versa.)
+            //Object reference is required
         }
 
+        static void LinqQuery()
+        {
+            var useLinq = from Properties in neighborhood
+
+                          
+        }
     }
 }
